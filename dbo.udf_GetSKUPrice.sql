326ecf26d3725b4a8539de610e4924ff6601d223
Egor Shashkov <130448114+Mortuus77@users.noreply.github.com>
2023-04-12 07:08:00 +0300

    Update dbo.udf_GetSKUPrice.sql

--- dbo.udf_GetSKUPrice.sql
+++ dbo.udf_GetSKUPrice.sql
@@ -3,8 +3,14 @@ RETURNS DECIMAL(18,2)
 AS
 BEGIN
     DECLARE @Cost DECIMAL(18,2);
-    SELECT @Cost = SUM(Value) / SUM(Quantity)
+    DECLARE @SumQuantity INT;
+    DECLARE @SumValue DECIMAL(18, 2);
+    SELECT @SumQuantity = SUM(Quantity),
+    @SumValue = SUM(Value)
     FROM dbo.Basket
     WHERE ID_SKU = @ID_SKU;
+    IF @SumQuantity > 0
+    SET @Cost = @SumValue / @SumQuantity
+    ELSE SET @Cost = 0
     RETURN @Cost;
 END;
